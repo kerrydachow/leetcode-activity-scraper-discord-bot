09d728b41c2654c3dbbe0a1d612b2d4f7ab6baab
kerrydachow <kerry_chow@hotmail.ca>
2023-09-18 20:49:15 -0700

    Add equals method to User

--- models/user.py
+++ models/user.py
@@ -3,10 +3,21 @@ from typing import Optional
 import json
 
 
-@dataclass
+@dataclass(eq=False)
 class User:
     """
     Class for keeping track of recent submission
     """
     username: str
     scraped_profile: Optional[json] = field(default=None, repr=False)
+
+    def __eq__(self, other) -> bool:
+        """
+        Configure equals method to compare the username of User.
+
+        :param other: other user
+        :return: bool
+        """
+        if not isinstance(other, User):
+            return False
+        return self.username == other.username
